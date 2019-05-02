8c9ed9879248e835fa19b12b815e5cc53d13ebd3
albagomezz <a.gomezde.2018@alumnos.urjc.es>
2019-05-02 22:47:16 +0200

    starting final project

--- server.py
+++ server.py
@@ -7,6 +7,15 @@ PORT = 8000
 socketserver.TCPServer.allow_reuse_address = True
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
-    
+
     def do_GET(self):
+        termcolor.cprint(self.requestline, 'green')
+        termcolor.cprint(self.command, 'yellow')
+        termcolor.cprint(self.path, 'red')
+
+        if self.path == '/' or self.path == '/main.html':
+            f = open("main.html", "r")
+            contents = f.read()
+            f.close()
+
 
